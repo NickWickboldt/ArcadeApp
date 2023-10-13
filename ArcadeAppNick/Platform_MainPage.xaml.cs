5e63e3a87a5e0a14e33b53e555ccd394f2a88760
NickWickboldt <nickwickboldt@gmail.com>
2023-10-13 11:47:23 -0500

    S3L3

--- ArcadeAppNick/Platform_MainPage.xaml.cs
+++ ArcadeAppNick/Platform_MainPage.xaml.cs
@@ -2,6 +2,7 @@ namespace ArcadeAppNick;
 
 public partial class Platform_MainPage : ContentPage
 {
+    private Player user; 
     public int level = 1;
     public int score = 0;
     public Label scoreLabel;
@@ -37,6 +38,20 @@ public partial class Platform_MainPage : ContentPage
                 unit.BackgroundColor = Colors.LightSkyBlue;
                 gameGrid.Add(unit, j, i);
 
+                if(i < 4 && j == 0)
+                {
+                    BoxView newRect = new BoxView()
+                    {
+                        HeightRequest = 20,
+                        Color = Colors.Green,
+                        VerticalOptions = LayoutOptions.End,
+                        CornerRadius = 10,
+                        ZIndex = 1
+                    };
+                    Platform newPlat = new Platform(newRect, i, j);
+                    gameGrid.Add(newRect, newPlat.col, newPlat.row);
+                }
+
                 await Task.Delay(100); 
             }
         }
@@ -65,7 +80,46 @@ public partial class Platform_MainPage : ContentPage
 
         await Task.Delay(100);
         gameGrid.Add(scoreLabel, 0, 4);
+        await Task.Delay(100); 
+        user = Create_User();
+        gameGrid.Add(user.image, user.col, user.row);
         await Task.Delay(100);
         gameGrid.Add(levelLabel, 4, 4);
+    }
+
+    private Player Create_User()
+    {
+        Image userIcon = new Image { Source = "dotnet_bot.png", ZIndex = 1 };
+        user = new Player(userIcon, 4, 2);
+        return user;
+    }
+}
+
+public class Player
+{
+    public Image image;
+    public int row;
+    public int col; 
+
+    public Player(Image i, int r, int c)
+    {
+        image = i; 
+        row = r;
+        col = c;
+    }
+}
+
+public class Platform
+{
+    public BoxView rect;
+    public int row;
+    public int col;
+
+    public Platform(BoxView rectangle, int r, int c)
+    {
+        this.rect = rectangle;
+        this.row = r;
+        this.col = c;
+
     }
 }
