46a6172c6720d3fefcfa756c47cb6e3ee80f4a26
NickWickboldt <nickwickboldt@gmail.com>
2024-01-28 21:45:53 -0600

    S7L3

--- ArcadeAppNick/TicTacToe.xaml.cs
+++ ArcadeAppNick/TicTacToe.xaml.cs
@@ -17,8 +17,171 @@ public partial class TicTacToe : ContentPage
 	public TicTacToe()
 	{
 		InitializeComponent();
+        InitializeGame(); 
+    }
+
+    private void Square0_Clicked(object sender, EventArgs e)
+    {
+        sq0.PlayerTurn();
+        currentTurn++; 
+        squares.Remove(sq0);
+        CheckForWin(); //checking 
+        RandomLocation();
+        CheckForWin(); //checking
+    }
+
+    private void Square1_Clicked(object sender, EventArgs e)
+    {
+        sq1.PlayerTurn();
+        currentTurn++; 
+        squares.Remove(sq1);
+        CheckForWin(); //checking 
+        RandomLocation();
+        CheckForWin(); //checking
+    }
+
+    private void Square2_Clicked(object sender, EventArgs e)
+    {
+        sq2.PlayerTurn();
+        currentTurn++; 
+        squares.Remove(sq2);
+        CheckForWin(); //checking 
+        RandomLocation();
+        CheckForWin(); //checking
+    }
+
+    private void Square3_Clicked(object sender, EventArgs e)
+    {
+        sq3.PlayerTurn();
+        currentTurn++;
+        squares.Remove(sq3);
+        CheckForWin(); //checking 
+        RandomLocation();
+        CheckForWin(); //checking
+    }
+
+    private void Square4_Clicked(object sender, EventArgs e)
+    {
+        sq4.PlayerTurn();
+        currentTurn++;
+        squares.Remove(sq4);
+        CheckForWin(); //checking 
+        RandomLocation();
+        CheckForWin(); //checking
+    }
+
+    private void Square5_Clicked(object sender, EventArgs e)
+    {
+        sq5.PlayerTurn();
+        currentTurn++;
+        squares.Remove(sq5);
+        CheckForWin(); //checking 
+        RandomLocation();
+        CheckForWin(); //checking
+    }
+
+    private void Square6_Clicked(object sender, EventArgs e)
+    {
+        sq6.PlayerTurn();
+        currentTurn++;
+        squares.Remove(sq6);
+        CheckForWin(); //checking 
+        RandomLocation();
+        CheckForWin(); //checking
+    }
+
+    private void Square7_Clicked(object sender, EventArgs e)
+    {
+        sq7.PlayerTurn();
+        currentTurn++;
+        squares.Remove(sq7);
+        CheckForWin(); //checking 
+        RandomLocation();
+        CheckForWin(); //checking
+    }
+
+    private void Square8_Clicked(object sender, EventArgs e)
+    {
+        sq8.PlayerTurn();
+        currentTurn++;
+        squares.Remove(sq8);
+        CheckForWin(); //checking 
+        RandomLocation();
+        CheckForWin(); //checking
+    }
+
+    public void RandomLocation()
+    {
+        if(squares.Count > 0)
+        {
+            var rand = new Random();
+            int loc = rand.Next(squares.Count);
+            
+            squares[loc].AITurn();
+            squares.Remove(squares[loc]);
+        }
+         
+    }
+
+    public void CheckForWin()
+    {       
+        if(sq0.isX && sq1.isX && sq2.isX ||     //Checking Xs Horizontal
+           sq3.isX && sq4.isX && sq5.isX ||
+           sq6.isX && sq7.isX && sq8.isX ||
+           
+           sq0.isX && sq3.isX && sq6.isX ||     //Checking Xs Vertical
+           sq1.isX && sq4.isX && sq7.isX ||
+           sq2.isX && sq5.isX && sq8.isX ||
+           
+           sq0.isX && sq4.isX && sq8.isX ||     //Checking Xs Diagonal
+           sq2.isX && sq4.isX && sq6.isX)
+        {
+            //X (user) has won
+            GameOver("user"); 
+        }
+        if (sq0.isO && sq1.isO && sq2.isO ||     //Checking Os Horizontal
+           sq3.isO && sq4.isO && sq5.isO ||
+           sq6.isO && sq7.isO && sq8.isO ||
+                                       
+           sq0.isO && sq3.isO && sq6.isO ||     //Checking Os Vertical
+           sq1.isO && sq4.isO && sq7.isO ||
+           sq2.isO && sq5.isO && sq8.isO ||
+                                       
+           sq0.isO && sq4.isO && sq8.isO ||     //Checking Os Diagonal
+           sq2.isO && sq4.isO && sq6.isO)
+        {
+            //O (AI) has won
+            GameOver("ai"); 
+        }
+    }
+
+    public void GameOver(string id)
+    {
+        if(squares.Count > 0 ) {
+            foreach (var boardsquare in squares)
+            {
+                boardsquare.square.IsEnabled = false; 
+            }
+        }
+        if(id == "user")
+        {
+            Winner_Label.Text = "You have won!";
+        }
+        else if(id == "ai")
+        {
+            Winner_Label.Text = "AI has won!"; 
+        }
+        New_Game_Button.IsVisible = true; 
+    }
+
+    public void InitializeGame()
+    {
+        squares.Clear(); //empty list
+        currentTurn = 0; //reset turns
+        New_Game_Button.IsVisible = false; 
+
         sq0 = new BoardSquare(Square0, 0);
-        squares.Add(sq0); 
+        squares.Add(sq0);
         sq1 = new BoardSquare(Square1, 1);
         squares.Add(sq1);
         sq2 = new BoardSquare(Square2, 2);
@@ -37,89 +200,15 @@ public partial class TicTacToe : ContentPage
         squares.Add(sq8);
     }
 
-    private void Square0_Clicked(object sender, EventArgs e)
+    private void New_Game_Button_Clicked(object sender, EventArgs e)
     {
-        sq0.PlayerTurn();
-        currentTurn++; 
-        squares.Remove(sq0);
-        RandomLocation();
-    }
-
-    private void Square1_Clicked(object sender, EventArgs e)
-    {
-        sq1.PlayerTurn();
-        currentTurn++; 
-        squares.Remove(sq1);
-        RandomLocation();
-    }
-
-    private void Square2_Clicked(object sender, EventArgs e)
-    {
-        sq2.PlayerTurn();
-        currentTurn++; 
-        squares.Remove(sq2);
-        RandomLocation();
-    }
-
-    private void Square3_Clicked(object sender, EventArgs e)
-    {
-        sq3.PlayerTurn();
-        currentTurn++;
-        squares.Remove(sq3);
-        RandomLocation();
-    }
-
-    private void Square4_Clicked(object sender, EventArgs e)
-    {
-        sq4.PlayerTurn();
-        currentTurn++;
-        squares.Remove(sq4);
-        RandomLocation();
-    }
-
-    private void Square5_Clicked(object sender, EventArgs e)
-    {
-        sq5.PlayerTurn();
-        currentTurn++;
-        squares.Remove(sq5);
-        RandomLocation();
-    }
-
-    private void Square6_Clicked(object sender, EventArgs e)
-    {
-        sq6.PlayerTurn();
-        currentTurn++;
-        squares.Remove(sq6);
-        RandomLocation();
-    }
-
-    private void Square7_Clicked(object sender, EventArgs e)
-    {
-        sq7.PlayerTurn();
-        currentTurn++;
-        squares.Remove(sq7);
-        RandomLocation();
-    }
-
-    private void Square8_Clicked(object sender, EventArgs e)
-    {
-        sq8.PlayerTurn();
-        currentTurn++;
-        squares.Remove(sq8);
-        RandomLocation();
-    }
-
-    public void RandomLocation()
-    {
-        if(squares.Count > 0)
+        Winner_Label.Text = ""; 
+        InitializeGame();
+        foreach (var boardsquare in squares)
         {
-            var rand = new Random();
-            int loc = rand.Next(squares.Count);
-            
-            squares[loc].AITurn();
-            squares.Remove(squares[loc]);
+            boardsquare.square.IsEnabled = true;
+            boardsquare.square.Source = ""; 
         }
-         
     }
 }
 
