a8f02fa51bcb3a25ce84e2490c76b1d781080fe0
NickWickboldt <nickwickboldt@gmail.com>
2024-02-08 14:25:37 -0600

    S7L5

--- ArcadeAppNick/Checkers.xaml.cs
+++ ArcadeAppNick/Checkers.xaml.cs
@@ -2,6 +2,12 @@ namespace ArcadeAppNick;
 
 public partial class Checkers : ContentPage
 {
+    //class attributes 
+    public List<CheckerboardSquare> CheckerBoard = new List<CheckerboardSquare>();
+    public List<Checker> AIPieces = new List<Checker>();
+    public List<Checker> UserPieces = new List<Checker>();
+    public bool checkerIsSelected = false; 
+    //class constructor
 	public Checkers()
 	{
 		InitializeComponent();
@@ -51,45 +57,126 @@ public partial class Checkers : ContentPage
                 if ((j == 0 || j == 1 || j == 2) && sq.BackgroundColor.Equals(new Color(0, 0, 0)))
                 {
                     sq.Source = "black_piece.png";
+                    Checker newPiece = new Checker(i, j);
+                    AIPieces.Add(newPiece); 
                 }
 
                 //create user pieces
                 if ((j == 5 || j == 6 || j == 7) && sq.BackgroundColor.Equals(new Color(0, 0, 0)))
                 {
                     sq.Source = "red_piece.png";
+                    Checker newPiece = new Checker(i, j);
+                    UserPieces.Add(newPiece); 
                 }
 
-                Gameboard.Add(sq, i, j); 
+                Gameboard.Add(sq, i, j);
+                CheckerBoard.Add(new CheckerboardSquare(this, sq, i, j)); 
             }
         }
+    }
+
+    public Checker IdentifyChecker(int[] location)
+    {
+        foreach (Checker piece in UserPieces)
+        {
+            if (piece.currentLocation[0] == location[0] && piece.currentLocation[1] == location[1])
+            {
+                return piece;
+            }
+        }
+        return null; 
+    }
+
+    public CheckerboardSquare IdentifyCheckerBoardSquare(int[] location)
+    {
+        foreach (CheckerboardSquare square in CheckerBoard)
+        {
+            if (square.location[0] == location[0] && square.location[1] == location[1])
+            {
+                return square;
+            }
+        }
+        return null; 
     }
 }
 public class Checker
 {
     public int[] currentLocation = new int[2];
-
+    
     public Checker(int i, int j)
     {
         currentLocation[0] = i;
-        currentLocation[1] = j;
+        currentLocation[1] = j; 
     }
 
 }
 
 public class CheckerboardSquare
 {
-    MainPage p;
-    private ImageButton square;
-    public int[] location = new int[2]; 
-    private bool isActive = false;
-    public bool choosingForMove;
+    Checkers p;
+    public ImageButton square;
+    public int[] location = new int[2];
+    public bool isActive = false;
+    public bool choosingForMove = false;
+    public int currentState = 0;
+    public EventHandler DoToggle;
+    public EventHandler DoMove; 
 
-    public CheckerboardSquare(int i, int j, ImageButton sq, MainPage page)
+    public CheckerboardSquare(Checkers page, ImageButton sq, int i, int j)
     {
-        p = page;
+        p = page; 
         location[0] = i;
         location[1] = j;
         square = sq;
-        choosingForMove = false;
+        TestActive(); 
+    }
+
+    public void AddBorder()
+    {
+        square.BorderColor = Color.FromRgb(255, 223, 0);
+        square.BorderWidth = 5;
+    }
+
+    public void RemoveBorder()
+    {
+        square.BorderColor = Color.FromRgb(0, 0, 0);
+        square.BorderWidth = 0;
+    }
+
+    public void TestActive()
+    {
+        if (Convert.ToString(square.Source).Length > 7)
+        {
+            isActive = true;
+            if (Convert.ToString(square.Source).Substring(6) == "red_piece.png")
+            {
+                DefineRedClick(); 
+            }
+        }
+        else
+        {
+            isActive = false; 
+        }
+    }
+
+    public void DefineRedClick()
+    {
+        DoToggle = (sender, args) =>    //arrow function ~ lambda expression
+        {
+            if(currentState == 0 && (p.checkerIsSelected == false))
+            {
+                square.Source = "red_piece_active.png";
+                choosingForMove = true; 
+                currentState = 1;   //toggle
+                p.checkerIsSelected = true; 
+            }else if (choosingForMove)
+            {
+                square.Source = "red_piece.png";
+                currentState = 0;
+                choosingForMove = false;
+                p.checkerIsSelected = false; 
+            }
+        };
+        square.Clicked += DoToggle; 
     }
 }
